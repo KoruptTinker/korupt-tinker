846409c3251f869e3134a4c698c0d40111d08212
KoruptTinker <pisces1237@gmail.com>
2021-06-11 00:26:53 +0530

    Final commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import Navibar from "./components/Navibar.js";
 import Home from "./components/Home.js";
 import About from "./components/About.js";
 import Education from "./components/Education.js";
@@ -9,7 +8,6 @@ import './App.css';
 function App() {
   return (
     <>
-    <Navibar></Navibar>
     <Home></Home>
     <About></About>
     <Education></Education>
